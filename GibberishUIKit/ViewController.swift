d29511c7d66866aa6793cf97d40390031a4c8ec1
Apoorv <apoorva.kumar@hdi.de>
2022-09-21 16:50:43 +0200

    Added demo UIKit code

--- GibberishUIKit/ViewController.swift
+++ GibberishUIKit/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel?
+    @IBOutlet weak var mainButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        outputLabel?.text = "Random initial text: \(Int.random(in: 42..<70))"
     }
 
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("button tapped")
+        let randomString = "A random number: \(Int.random(in: 1...100))"
+        outputLabel?.text = "\(randomString) for label!"
+        mainButton?.setTitle(randomString, for: .normal)
+    }
 }
 
